fa31b4ee198c7052674fde5aa7e9b1f0d4b90806
EdsonFufu <edsonfufu@NMB-HQ101-MAC046.local>
2022-09-17 00:59:44 +0300

    extend SpringBootServletInitializer to application

--- src/main/java/com/simplilearn/project/onlinebankapp/IcinOnlineBankAppApplication.java
+++ src/main/java/com/simplilearn/project/onlinebankapp/IcinOnlineBankAppApplication.java
@@ -2,13 +2,14 @@ package com.simplilearn.project.onlinebankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class IcinOnlineBankAppApplication {
+public class IcinOnlineBankAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(IcinOnlineBankAppApplication.class, args);
